48f1b560823db16ef9a8ae71664ffe11968c641b
marizal-akib <marizalakib@gmail.com>
2024-03-09 03:33:00 +0000

    first commit

---
+++ src/Components/Pages/Home/Home.jsx
@@ -0,0 +1,15 @@
+import Footer from "../../Common/Footer";
+import Nav from "../../Common/Nav";
+
+
+const Home = () => {
+    return (
+        <div>
+            <Nav></Nav>
+            <p>My home</p>
+            <Footer></Footer>
+        </div>
+    );
+};
+
+export default Home;
